5863d1d49f773d0b54fe705571d26437d9e19676
krivov <sergey@krivov.info>
2015-09-12 11:43:04 +0300

    save map view region properties

--- VirtualTourist/MapViewController.swift
+++ VirtualTourist/MapViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import MapKit
+import CoreData
 
 class MapViewController: UIViewController, MKMapViewDelegate {
     
@@ -23,6 +24,8 @@ class MapViewController: UIViewController, MKMapViewDelegate {
         mapView.delegate = self
         
         self.deleteAlert.hidden = true
+        
+        self.loadMapViewRegion()
     }
     
     @IBAction func longPress(sender: UILongPressGestureRecognizer) {
@@ -42,6 +45,60 @@ class MapViewController: UIViewController, MKMapViewDelegate {
             self.isEditButtonTapped = true
         }
     }
-
+    
+    // ====================================================================================
+    // MARK: - Core Data
+    
+    // CoreData sharedContext
+    var sharedContext: NSManagedObjectContext {
+        return CoreDataStackManager.sharedInstance().managedObjectContext!
+    }
+    
+    // ====================================================================================
+    // MARK: - NSUserDefaults constants and methods
+    
+    let CenterLatitudeMapView     = "Center Latitude"
+    let CenterLongitudeMapView    = "Center Longitude"
+    let SpanLatitudeDeltaMapView  = "Span Latitude Delta"
+    let SpanLongitudeDeltaMapView = "Span Longitude Delta"
+    
+    //Save mapView region properties to NSUserDefaults
+    func saveMapViewRegion() {
+        NSUserDefaults.standardUserDefaults().setDouble(mapView.region.center.latitude, forKey: CenterLatitudeMapView)
+        NSUserDefaults.standardUserDefaults().setDouble(mapView.region.center.longitude, forKey: CenterLongitudeMapView)
+        NSUserDefaults.standardUserDefaults().setDouble(mapView.region.span.latitudeDelta, forKey: SpanLatitudeDeltaMapView)
+        NSUserDefaults.standardUserDefaults().setDouble(mapView.region.span.longitudeDelta, forKey: SpanLongitudeDeltaMapView)
+    }
+    
+    //Reload mapView region properties from NSUserDefaults
+    func loadMapViewRegion() {
+        
+        //if mapView region properties exist in NSUserDefaults - zoom map to save location
+        let centerLatitude = NSUserDefaults.standardUserDefaults().doubleForKey(CenterLatitudeMapView)
+        
+        if centerLatitude != 0 {
+            
+            //Get all map view region properties
+            let centreLongitude = NSUserDefaults.standardUserDefaults().doubleForKey(CenterLongitudeMapView)
+            let spanLatitudeDelta = NSUserDefaults.standardUserDefaults().doubleForKey(SpanLatitudeDeltaMapView)
+            let spanLongitudeDelta = NSUserDefaults.standardUserDefaults().doubleForKey(SpanLongitudeDeltaMapView)
+            
+            let center = CLLocationCoordinate2DMake(centerLatitude, centreLongitude)
+            let span = MKCoordinateSpanMake(spanLatitudeDelta, spanLongitudeDelta)
+            
+            //set properties to map view
+            let region = MKCoordinateRegionMake(center, span)
+            mapView.region = region
+        }
+    }
+    
+    // ====================================================================================
+    // MARK: - MKMapViewDelegate implementation
+    
+    func mapView(mapView: MKMapView!, regionDidChangeAnimated animated: Bool) {
+        
+        //Save map view region properties
+        saveMapViewRegion()
+    }
 }
 
